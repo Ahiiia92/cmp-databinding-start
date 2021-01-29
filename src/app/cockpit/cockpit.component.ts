2e20ba084ac9bfaded8ac8d131fd8372ffa8edbd
Maroussia Arnault <maroussia.arnault@gmail.com>
2021-01-29 18:24:55 +0100

    Local References

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -16,7 +16,8 @@ export class CockpitComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  onAddServer() {
+  onAddServer(nameInput) {
+    console.log(nameInput);
     this.serverCreated.emit({
       serverName: this.newServerName,
       serverContent: this.newServerContent
